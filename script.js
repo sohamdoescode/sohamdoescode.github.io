ee066fe3923ff954a5723e7fdcd6a08d3bb31342
Soham Bhattacharyya <soham.b630@gmail.com>
2024-05-28 12:17:22 +0530

    Update words, increase typing effect time

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-var i = 0;
-var txt = 'Soham Bhattacharyya'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typer").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+const words = [
+    "Full Stack Developer",
+    "React.js",
+    "Node.js",
+    "MongoDB",
+    "Tailwind CSS"
+];
+let i = 0;
+let counter;
+function typeNow() {
+    let word = words[i].split("");
+    var loopTyping = function () {
+        if (word.length > 0) {
+            document.getElementById("typer").innerHTML += word.shift();
+        } else {
+            deleteNow();
+            return false;
+        }
+        counter = setTimeout(loopTyping, 225);
+    };
+    loopTyping();
 }
+function deleteNow() {
+    let word = words[i].split("");
+    var loopDeleting = function () {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById("typer").innerHTML = word.join("");
+        } else {
+            if (words.length > i + 1) {
+                i++;
+            } else {
+                i = 0;
+            }
+            typeNow();
+            return false;
+        }
+        counter = setTimeout(loopDeleting, 100);
+    };
+    loopDeleting();
+}
+typeNow();
